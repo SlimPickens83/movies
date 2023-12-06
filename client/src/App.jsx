d5fc421b3862f41f0c382399519d0c5184cc6563
SlimPickens83 <mikerad83@gmail.com>
2023-12-06 16:18:35 -0500

    Added styling and navigation bar to main page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,59 @@ import HomeGuest from "./components/HomeGuest"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 
+import Button from "react-bootstrap/Button"
+import Container from "react-bootstrap/Container"
+import Form from "react-bootstrap/Form"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
+import NavDropdown from "react-bootstrap/NavDropdown"
+
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+      <style type="text/css">{`
+        .dropdown-menu, .dropdown-item {
+          background-color: #100000;
+          color: #f7f6f2;
+        }
+        .nav-link, .navbar-nav, .nav-link.show, .btn {
+          color: #f7f6f2 !important;
+          border-color: #f7f6f2 !important;
+        }
+        .btn-outline-success {
+          --bs-btn-color: #e000000 !important;
+          --bs-btn-border-color: #e000000 !important;
+          --bs-btn-hover-bg: #e000000 !important;
+          --bs-btn-hover-border-color: #e000000 !important;
+          --bs-btn-active-bg: #e000000 !important;
+          --bs-btn-active-border-color: #e000000 !important;
+        }
+      `}</style>
+      <Navbar id="navbar" expand="lg">
+        <Container fluid>
+          <Navbar.Brand id="brandLink" style={{ color: "#f7f6f2" }} href="#">
+            Movie Library Plus
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {/* <Header /> */}
       <Routes>
         <Route path="/" element={<HomeGuest />} />
       </Routes>
--- client/src/components/HomeGuest.jsx
+++ client/src/components/HomeGuest.jsx
@@ -1,11 +1,11 @@
 import React from "react"
-import backgroundImage from "../assets/theaterSeatsBlank.jpeg"
+// import backgroundImage from "../assets/theaterSeatsBlank.jpeg"
 
 function HomeGuest() {
   return (
     <div id="homeGuest">
       <h1>Welcome to Your New Movie Library</h1>
-      <img id="theaterSeats" src={backgroundImage} alt="background" />
+      {/* <img id="theaterSeats" src={backgroundImage} alt="background" /> */}
     </div>
   )
 }
